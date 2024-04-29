cb3c504a42dc26646238f2c507d4de4eb7d0e347
mraghul <154625635+mraghul@users.noreply.github.com>
2024-04-29 21:50:52 +0530

    E-commerce website using HTML,CSS & BOOTSTRAP

---
+++ E-Commerce/js/main.js
@@ -0,0 +1,19 @@
+// let menu = document.querySelector("#menu-bar");
+// let navBar = document.querySelector(".navbar-1");
+
+// menu.onclick = () =>{
+//     menu.classList.toggle("fa-times");
+//     navBar.classList.toggle("active");
+// }
+
+
+// window.onscroll = () =>{
+//     menu.classList.toggle("fa-times");
+//     navBar.classList.toggle("active");
+
+//     if(window.scrollY > 50){
+//         document.querySelector("#scroll-top").classList.add('active')
+//     }else{
+//         document.querySelector("#scroll-top").classList.remove('active')
+//     }
+// }
